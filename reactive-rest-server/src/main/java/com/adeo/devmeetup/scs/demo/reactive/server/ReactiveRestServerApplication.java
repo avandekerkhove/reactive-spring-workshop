0ce5b5dced1c7fe1b4c8a53f70cd9f0565f57d97
Alexandre Vandekerkhove <a.vandekerkhove@gmail.com>
2018-06-24 18:39:01 +0200

    Initial commit of reactive rest server

---
+++ reactive-rest-server/src/main/java/com/adeo/devmeetup/scs/demo/reactive/server/ReactiveRestServerApplication.java
@@ -0,0 +1,12 @@
+package com.adeo.devmeetup.scs.demo.reactive.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestServerApplication.class, args);
+	}
+}
---
+++ reactive-rest-server/src/test/java/com/adeo/devmeetup/scs/demo/reactive/server/ReactiveRestServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adeo.devmeetup.scs.demo.reactive.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveRestServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
