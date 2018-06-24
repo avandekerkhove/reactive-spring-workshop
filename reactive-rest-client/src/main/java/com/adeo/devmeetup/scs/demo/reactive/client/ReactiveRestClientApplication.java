ff90d023829c0d8fc8410172e3c2bfa7253ae64b
Alexandre Vandekerkhove <a.vandekerkhove@gmail.com>
2018-06-25 01:39:47 +0200

    Initial commit of reactive-rest-client

---
+++ reactive-rest-client/src/main/java/com/adeo/devmeetup/scs/demo/reactive/client/ReactiveRestClientApplication.java
@@ -0,0 +1,12 @@
+package com.adeo.devmeetup.scs.demo.reactive.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveRestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveRestClientApplication.class, args);
+	}
+}
